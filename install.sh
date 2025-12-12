72f470d1c0f03d7dc3ef0d2c5ea99efb93825525
sakievmi-dev <sakievmi@proton.me>
2025-12-12 14:04:25 +0200

    forgot to make clone repo

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo pacman -S git
+
 #   CHECKING IF PLASMA IS RUNNING
 if [[ "${XDG_CURRENT_DESKTOP:-}" != *KDE* && "${XDG_CURRENT_DESKTOP:-}" != *Plasma* ]]; then
     echo "Error: KDE Plasma is not running. Aborting."
@@ -9,10 +11,14 @@ if [[ "${XDG_CURRENT_DESKTOP:-}" != *KDE* && "${XDG_CURRENT_DESKTOP:-}" != *Plas
 fi
 
 #   INSTALLING SCRIPTS
+git clone https://github.com/sakievmi-dev/fubuki-plasma /tmp/fubuki-plasma
+
 echo "Installing scripts into /usr/bin..."
 
 sudo rm ~/.local/bin/fubuki-install
 sudo rm ~/.local/bin/fubuki-cheatsheet
 
-install -m 755 scripts/fubuki-install.sh ~/.local/bin/fubuki-install
-install -m 755 scripts/fubuki-cheatsheet.sh ~/.local/bin/fubuki-cheatsheet
+install -m 755 /tmp/fubuki-plasma/scripts/fubuki-install.sh ~/.local/bin/fubuki-install
+install -m 755 /tmp/fubuki-plasma/scripts/fubuki-cheatsheet.sh ~/.local/bin/fubuki-cheatsheet
+
+rm -rf /tmp/fubuki-plasma
